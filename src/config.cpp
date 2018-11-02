aefde1267492e86d9090fba6a23f47140dda06f0
Varun Patil <radialapps@gmail.com>
2018-11-02 16:12:25 +0530

    Allow fallback to system resolution for window width/height

--- src/config.cpp
+++ src/config.cpp
@@ -1,9 +1,19 @@
 #include "config.hpp"
 #include <fstream>
+#include <SFML/Window.hpp>
 
 Config::Config() {
     std::ifstream ifs(CONFIG_PATH);
     conf = nlohmann::json::parse(ifs);
+
+    /* Set fallbacks for width and height */
+    if (this->getI(K_WINDOW_WIDTH) == 0) {
+        conf[K_WINDOW_WIDTH] = sf::VideoMode::getDesktopMode().width;
+    }
+
+    if (this->getI(K_WINDOW_HEIGHT) == 0) {
+        conf[K_WINDOW_HEIGHT] = sf::VideoMode::getDesktopMode().height;
+    }
 }
 
 #define RETURN {\
