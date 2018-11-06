eecfdb60e4051d972c052fb39e16c06e08da562e
Varun Patil <radialapps@gmail.com>
2018-11-06 11:13:09 +0530

    Document config params in config.hpp

--- src/config.hpp
+++ src/config.hpp
@@ -14,28 +14,28 @@
 #define CONFIG_PATH "config.json"
 
 /* Key definitions */
-#define K_LOCATION "location"
+#define K_LOCATION "location"                           // Location of billboard, sent in network requests
 
-#define K_WINDOW_WIDTH "window_width"
-#define K_WINDOW_HEIGHT "window_height"
-#define K_WINDOW_TITLE "window_title"
-#define K_FULLSCREEN "fullscreen"
-#define K_TIME_DELAY "time_delay"
-#define K_EVENTS_URL "url_events"
-#define K_IMAGES_DIR "images_dir"
-#define K_CACHE_DIR "cache_dir"
-#define K_REFRESH_DURATION "refresh_duration"
-#define K_WEIGHT_THRESHOLD "weight_threshold"
+#define K_WINDOW_WIDTH "window_width"                   // Window width in pixels, set to 0 for native
+#define K_WINDOW_HEIGHT "window_height"                 // Window height in pixels, set to 0 for native
+#define K_WINDOW_TITLE "window_title"                   // Window height in pixels, set to 0 for native
+#define K_FULLSCREEN "fullscreen"                       // Set true for fullscreen
+#define K_TIME_DELAY "time_delay"                       // Time delay between transitions for EventScene
+#define K_EVENTS_URL "url_events"                       // URL to fetch events
+#define K_IMAGES_DIR "images_dir"                       // Directory to cache images in (should exist necessarily)
+#define K_CACHE_DIR "cache_dir"                         // Directory to cache API responses (should exist necessarily)
+#define K_REFRESH_DURATION "refresh_duration"           // Refresh events every x seconds
+#define K_WEIGHT_THRESHOLD "weight_threshold"           // Don't display events below this weight
 
-#define K_ANIMATION_ENABLED "animation_enabled"
-#define K_EVENT_ANIMATION_SPEED "animation_speed"
+#define K_ANIMATION_ENABLED "animation_enabled"         // Set to false to disable animations
+#define K_EVENT_ANIMATION_SPEED "animation_speed"       // Animation transition speed for EventScene
 
-#define K_LOGO_SUPER "logo_super"
-#define K_LOGO_NAME "logo_name"
-#define K_LOGO_ANIM_SPEED "logo_anim_speed"
-#define K_LOGO_REPEAT_DELAY "logo_repeat_delay"
-#define K_LOGO_SHOW_DURATION "logo_show_duration"
-#define K_LOGO_FADE_SPEED "logo_fade_speed"
+#define K_LOGO_SUPER "logo_super"                       // Text to display above logo (small)
+#define K_LOGO_NAME "logo_name"                         // Title to display below logo (large)
+#define K_LOGO_ANIM_SPEED "logo_anim_speed"             // Logo circle expand animation speed
+#define K_LOGO_REPEAT_DELAY "logo_repeat_delay"         // Repeat showing logo after every x seconds
+#define K_LOGO_SHOW_DURATION "logo_show_duration"       // Show logo for x seconds
+#define K_LOGO_FADE_SPEED "logo_fade_speed"             // Speed of fading logo in/out
 
 /* Short key definitions */
 #define K_W "window_width"
